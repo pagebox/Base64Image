432c40e5c6a7fd74284b48ca6dab6fd20c8d396a
Patrick Gehrig <pagebox@hotmail.com>
2021-11-11 22:37:38 +0100

    INotifyPropertyChanged

--- Base64Image/Image.cs
+++ Base64Image/Image.cs
@@ -1,10 +1,40 @@
 ﻿
+using System.ComponentModel;
+
 namespace Base64Image {
-    class Image {
+    internal class Image : INotifyPropertyChanged {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
 
         public System.Windows.Media.Imaging.BitmapSource Source { get; set; }
         public string Status { get; set; }
-        public string Text { get; set; }
+
+
+        private string text;
+        public string Text {
+            get => text;
+            set {
+                text = value;
+                OnPropertyChanged("Text");
+            }
+        }
+
+
+
+        private string format;
+
+        public string Format {
+            get { return format; }
+            set {
+                format = value;
+                OnPropertyChanged("Format");
+            }
+        }
+
 
     }
 }
